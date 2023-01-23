6f29fe230e6c17c9b0d92c4179c51801ef3b1269
aaronsx <senen.00@gmail.com>
2023-01-23 09:37:55 +0100

    no message

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/Tipos-de-datos.js
@@ -0,0 +1,83 @@
+
+function sabertipo()
+{
+	 let variable1 = 10;
+     let variable2 = 5.3;
+
+      document.write(typeof(variable1));
+      document.write(typeof variable2);
+}
+function verdaderoyfalso()
+{
+	   let variable1 = 10;
+     let variable2 = 5.3;
+
+      document.write(typeof(variable1));
+      document.write(typeof variable2);
+}
+function numbernormal()
+{
+	   let variable1 = 10;
+     let variable2 = 5.3;
+
+      document.write(typeof(variable1));
+      document.write(typeof variable2);
+}
+function numerogrande()
+{
+	 let numeroGrande = 4567891324657987654;
+     let numeroGrandeCasteado = BigInt(987654321654987654);
+
+     document.write(typeof(numeroGrande));
+     document.write(numeroGrande);
+     document.write(typeof(numeroGrandeCasteado));
+     document.write(numeroGrandeCasteado);
+}
+
+function texto()
+{
+	 let texto = "Soy un string";
+
+	 document.write(typeof(texto));
+     document.write(texto);
+}
+function vacio()
+{
+	 let variableVacia;
+	 
+     document.write(variableVacia);
+     document.write(typeof(variableVacia));
+}
+function nulo()
+{
+	 let nulo = null;
+     
+     document.write(nulo);
+     document.write(typeof(nulo));
+}
+function Objecto()
+{
+	 let Objeto = new Object();
+    
+	document.write(Objeto);
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
