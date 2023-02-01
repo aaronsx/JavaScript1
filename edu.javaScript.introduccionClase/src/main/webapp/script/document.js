7345feaf01488c8e41d05f90e3327fb90b3ca64b
aaronsx <senen.00@gmail.com>
2023-02-01 09:46:17 +0100

    Operador

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/document.js
@@ -0,0 +1,39 @@
+
+
+
+
+function cambiaColor(colorin)
+{
+
+ document.bgColor = colorin 
+ 
+}
+
+function cambiaTitulo(texto)
+{ 
+    document.title = texto 
+ 
+}
+function link ()
+{
+	let url = document.links[0].href;
+	let fun= document.links.namedItem;
+	let num=document.links.item
+	document.getElementById("link").innerHTML = url;
+	document.getElementById("link").innerHTML = fun;
+}
+/*function cookie()
+{
+	cokie="Aaron"
+	document.cookie ="cokie=Aaron; path=/"
+	document.write(document.cookie.)
+	
+}*/
+/*function location()
+{
+	document.write(document.location)
+}
+console.log(document.location);*/
+
+
+	
---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/operacionesArismetica.js
@@ -0,0 +1,22 @@
+function ejemplo1()
+{
+	let suma=10.25 + 0.75;
+	document.write(typeof suma);
+}
+function ejemplo2()
+{
+	let numero1 = 23;
+	let numero2 = 34;
+	let resultadoOperacion = numero1 +numero2;
+	document.write(resultadoOperacion);
+}
+function ejemplo3()
+{
+	let operacion = (46 + 8) * 2 / 2 + 46;
+	document.write(operacion);
+}
+function ejemplo4()
+{
+	let operacion = 46 + 8 * 2 / (2 + 46);
+	document.write(operacion);
+}
